ee088c0577b835b0c6166ecaefd97d27c72a6e2a
Andy Chu <andy@oilshell.org>
2020-03-10 12:55:08 -0700

    ok

--- test/parse-errors.sh
+++ test/parse-errors.sh
@@ -218,9 +218,14 @@ arith-expr() {
   #_error-case '$(( ${var} = fd ))'
 
   # Invalid assignments
-  _error-case '$(( x+1 = fd ))'
+  _error-case '$(( x+1 = 42 ))'
   _error-case '$(( (x+42)++ ))'
   _error-case '$(( ++(x+42) ))'
+
+  # Note these aren't caught because '1' is an ArithWord like 0x$x
+  _error-case '$(( 1 = foo ))'
+  _error-case '$(( 1++ ))'
+  _error-case '$(( ++1 ))'
 }
 
 command-sub() {
