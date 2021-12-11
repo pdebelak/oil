7d22d13fb2cb0622f4ac3bcfa5b0feca8fc2035a
Andy C <andy@lenny>
2021-12-10 21:39:46 -0500

    ok try this

--- soil/deps-tar.sh
+++ soil/deps-tar.sh
@@ -7,12 +7,37 @@ set -o nounset
 set -o pipefail
 set -o errexit
 
+REPO_ROOT=$(cd $(dirname $0)/.. && pwd)
+readonly REPO_ROOT
+
+DEPS_DIR=$REPO_ROOT/../oil_DEPS
+readonly DEPS_DIR
+
+download-re2c() {
+  # local cache of remote files
+  mkdir -p _cache
+  wget --no-clobber --directory _cache \
+    https://github.com/skvadrik/re2c/releases/download/1.0.3/re2c-1.0.3.tar.gz
+}
+
+build-re2c() {
+  cd $REPO_ROOT/_cache
+  tar -x -z < re2c-1.0.3.tar.gz
+
+  mkdir -p $DEPS_DIR/re2c
+  cd $DEPS_DIR/re2c
+  $REPO_ROOT/_cache/re2c-1.0.3/configure
+  make
+}
+
 cpp() {
-  echo 'TODO: re2c'
+  download-re2c
+  build-re2c
 }
 
 ovm-tarball() {
-  echo 'TODO: re2c and more'
+  download-re2c
+  build-re2c
 }
 
 "$@"
